ad679250d1fb331817b8cb6cdf2c6855f6bba457
Crisóstomos Santos Silva <criscgweb@gmail.com>
2020-10-16 15:16:39 -0300

    Modularizando a aplicação: Lazy-loading, módulos compartilhados incluindo serviços, Feature module, Core module, preloadingStrategy.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID  } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'
 import { HomeComponent } from './home/home.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { MenuComponent } from './restaurant-detaill/menu/menu.component';
 import { MenuItemComponent } from './restaurant-detaill/menu-item/menu-item.component';
 import { ShoppingCartComponent } from './restaurant-detaill/shopping-cart/shopping-cart.component';
 import { RestaurantDetaillComponent } from './restaurant-detaill/restaurant-detaill.component';
 import { ReviewsComponent } from './restaurant-detaill/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detaill/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -23,26 +20,23 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,    
+    HomeComponent,
     RestaurantsComponent,
     RestaurantComponent,
     MenuComponent,
     MenuItemComponent,
     ShoppingCartComponent,
     RestaurantDetaillComponent,
-    ReviewsComponent,    
+    ReviewsComponent,
     OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,    
-    SharedModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),    
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules} ),
   ],
-  providers: [RestaurantsService, 
-              ShoppingCartService, 
-              OrderService,
-              {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
+
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
+
+import { ShoppingCartService } from "app/restaurant-detaill/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
+
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
@@ -10,16 +16,21 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     imports:[
             CommonModule, 
             FormsModule,
-            ReactiveFormsModule,
-            FormsModule,
-            ReactiveFormsModule, ],
+            ReactiveFormsModule ], 
 
     exports:[InputComponent, 
             RadioComponent, 
             RatingComponent, 
-            ReactiveFormsModule]
+            CommonModule,    
+            ReactiveFormsModule] 
 })
 
 export class SharedModule{
+        static forRoot(): ModuleWithProviders{
+                return{
+                        ngModule: SharedModule,
+                        providers:[ShoppingCartService, RestaurantsService, OrderService]
+                }
+        }
 
 }
